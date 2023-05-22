576cb2ba42cc6f766182ea26b05a4e3112eef464
hamza-mateen1 <hamzamateen57@gmail.com>
2023-05-22 22:59:49 +0500

    Class 2

---
+++ Class 2/app.js
@@ -0,0 +1,63 @@
+// let data1 = "Hello ";
+// let data2 = "Students";
+
+// let data3 = data1 + data2; // String Concatenation
+// console.log(data3);
+
+
+//num + num = addition
+// num + string = concatenation
+
+// let data1 = "5";
+// let data2 = 5;
+
+// let data3 = data1 + data2;
+// console.log(data3);
+
+
+
+// Case sensitive Language
+
+// let data = "Hello Students";
+// let data2 = data.includes("h");
+// console.log(data2);
+
+//Math Operators : = + - / %
+
+//Parsing
+// let data1 = "5";
+// let data2 = 5;
+// console.log(+data1 + data2);
+
+// let data1 = "5";
+// let data2 = 5;
+// console.log(data1 - data2);
+
+// let data1 = 17;
+// let data2 = 2;
+// console.log(data1 % data2);
+
+// Prompt
+
+// let FirstName = prompt("What is your Name");
+// console.log("Welcome " + FirstName);
+// document.write("Welcome " + FirstName);
+
+
+// Generating Random Number
+
+// let num = Math.random() * 10;
+// console.log(num);
+// num *= 10;  //Short hand
+
+
+// let num2 = Math.round(4.3);
+
+// console.log(num2);
+
+
+// BODMAS RULE
+
+// let num = (5 + 5) * (5/5);
+// let num2 = 5 ** 3;
+// console.log(num2);
