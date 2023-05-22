0e13f5f3982040a6f6dd71211d5b58b05c0edc17
hamza-mateen1 <hamzamateen57@gmail.com>
2023-05-22 13:11:08 +0500

    Class 1

---
+++ Class 1/app.js
@@ -0,0 +1,26 @@
+
+let firstName = "Hamza";
+alert("Hello SMIT");
+alert(firstName);
+
+// Variable rules
+
+
+//legal Names
+let lastName  //CamelCase
+let last_name  //SnakeCase
+two Special Characters Allowed : _ , $
+let myName1  : // variable name cannot be start with a number
+
+
+// illegal Names
+spaces between variable name
+let Name#First   : Do not use any special Character 
+do not use reserved words in variable  for example : alert
+
+
+//Datatype
+
+// String  let name = "Hamza"; any letter or alphabet or any number in quotation
+// number  let num = 8;  num = "8"
+// boolean  true and false
